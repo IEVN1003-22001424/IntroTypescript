618720fa264037d548d69a342c411db8a0334f26
ehernandez <82061@alumnos.utleon.edu.mx>
2026-09-04 20:27:23 -0600

    TiposDeDatos04/09/26

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("Hola Mundo UwU");
+ 
+var num1=23;
+let num2=65;
+const num3=22;
+ 
+let nombre:string;
+let num:number;
+let activo:boolean;
+let sum:number=num1+num2+num3;
+console.log(sum)
+
+nombre= "Pedro";
+console.log(nombre);
+
+nombre=4523;
+console.log(nombre);
+
